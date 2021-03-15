c1f8a543ea0da3e37bf31599b76188c779bef3d9
jorgevillaverde <jorgevillaverde@gmail.com>
2021-03-15 14:56:12 -0300

    add cache and event steam def to application

--- api/v1alpha1/application_types.go
+++ api/v1alpha1/application_types.go
@@ -20,12 +20,50 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// EventStreamType are the types of event stream supported
+type EventStreamType string
+
+const (
+	// EventStreamTypeKafka use Kafka for Event Stream
+	EventStreamTypeKafka EventStreamType = "Kafka"
+	// EventStreamTypeRabbitMQ use RabbitMQ for Event Stream
+	EventStreamTypeRabbitMQ EventStreamType = "RabbitMQ"
+)
+
+// EventStreamSpec is the definition for Event Stream support for and applicaction
+type EventStreamSpec struct {
+	// Type is the type of the cache
+	Type EventStreamType `json:"type,omitempty"`
+}
+
+// CacheType are the types of cache supported
+type CacheType string
+
+const (
+	// CacheTypeReddis use Reddis for Cache
+	CacheTypeReddis CacheType = "Reddis"
+	// CacheTypeMemcached use Memcached for Cache
+	CacheTypeMemcached CacheType = "Memcached"
+)
+
+// CacheSpec is the definition for Cache support for and applicaction
+type CacheSpec struct {
+	// Type is the type of the cache
+	Type CacheType `json:"type,omitempty"`
+}
+
 // ApplicationSpec defines the desired state of Application
 type ApplicationSpec struct {
-	// Name is the name of the application
-	Name string `json:"name,omitempty"`
 	// Description is the name of the application
 	Description string `json:"description,omitempty"`
+
+	// CacheRef is the reference to cache informacion for the applicacion
+	// +optional
+	CacheRef *CacheSpec `json:"cacheRef,omitempty"`
+
+	// EventStreamRefs is the reference to event stream informacion for the applicacion
+	// +optional
+	EventStreamRefs *EventStreamSpec `json:"eventStreamRef,omitempty"`
 }
 
 // ApplicationStatus defines the observed state of Application
